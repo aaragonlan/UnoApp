8afe106f2f4bd03b72caabbde3a6efbd6c5e4fd1
aaragonlan <aaragon@proxya.es>
2023-05-08 14:11:48 +0200

    mvn
    mvn clean install

--- src/main/java/es/proyecto/uno/UnoApp/UnoAppApplication.java
+++ src/main/java/es/proyecto/uno/UnoApp/UnoAppApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UnoAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("== START ==");
 		SpringApplication.run(UnoAppApplication.class, args);
+		System.out.println("=== START ===");
 	}
 
 }
