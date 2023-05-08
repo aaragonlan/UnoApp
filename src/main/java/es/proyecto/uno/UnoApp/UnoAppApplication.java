4c7fa02b14922c692c43a39741b877d9f1988f69
aaragonlan <aaragon@proxya.es>
2023-05-08 13:59:51 +0200

    Spring unpack

---
+++ src/main/java/es/proyecto/uno/UnoApp/UnoAppApplication.java
@@ -0,0 +1,13 @@
+package es.proyecto.uno.UnoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/proyecto/uno/UnoApp/UnoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package es.proyecto.uno.UnoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
